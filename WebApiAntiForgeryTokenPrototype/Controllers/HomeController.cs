a7dda04e845981886aba2189d5e3c255973f0b0b
rmbrunet <rmbrunet@gmail.com>
2013-01-31 15:15:23 -0500

    First Commit

---
+++ WebApiAntiForgeryTokenPrototype/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiAntiForgeryTokenPrototype.Controllers
+{
+
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [GenerateHttpHeaderAntiForgeryAttribute]
+        public HttpResponseMessage Tokens() {
+            //Just returning the Antiforgery cookie and header tokens. 
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+        
+        [HttpPost]
+        [HttpHeaderAntiForgeryTokenAttribute]
+        [GenerateHttpHeaderAntiForgeryAttribute]
+        public HttpResponseMessage Data([FromBody]string data) {
+            //At this point the Antiforgery Token has been validated by the filter
+            //Do something with the data here
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ WebApiAntiForgeryTokenPrototype/Filters/HttpHeaderAntiForgeryTokenAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Helpers;
+
+namespace WebApiAntiForgeryTokenPrototype {
+    public class HttpHeaderAntiForgeryTokenAttribute : System.Web.Http.Filters.AuthorizationFilterAttribute {
+        static string _tokenKey = AntiForgeryConfig.CookieName;
+
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext) {
+            if (actionContext == null) {
+                throw new ArgumentNullException("actionContext");
+            }
+
+            try {
+                CookieHeaderValue cookie = actionContext.Request.Headers.GetCookies(_tokenKey).FirstOrDefault();
+                var cookieToken = cookie != null ? cookie[_tokenKey].Value : null;
+
+                string headerToken = actionContext.Request.Headers.GetValues(_tokenKey).First();
+                System.Web.Helpers.AntiForgery.Validate(cookieToken, headerToken); // Will throw HttpAntiForgeryException
+            }
+            catch {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest) {
+                    Content = new StringContent("Invalid or Missing AntiForgery Token")
+                };
+            }
+            return;
+        }
+
+    }
+}
