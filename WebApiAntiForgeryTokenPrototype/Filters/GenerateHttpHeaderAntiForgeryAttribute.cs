471caecb32d344eb6116ef6af9d004bce4c10214
rmbrunet <rmbrunet@gmail.com>
2013-02-01 08:50:07 -0500

    Improved the Generate... Attribute

--- WebApiAntiForgeryTokenPrototype/Filters/GenerateHttpHeaderAntiForgeryAttribute.cs
+++ WebApiAntiForgeryTokenPrototype/Filters/GenerateHttpHeaderAntiForgeryAttribute.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net.Http; //HttpRequestMessageExtensions
 using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
+using System.Web;
 using System.Web.Helpers;
 
 namespace WebApiAntiForgeryTokenPrototype {
@@ -10,16 +11,31 @@ namespace WebApiAntiForgeryTokenPrototype {
     /// This attribute must generate the Anti Forgery tokens and send them to the client.
     /// </summary>
     public class GenerateHttpHeaderAntiForgeryAttribute : System.Web.Http.Filters.ActionFilterAttribute {
-        static string _tokenKey = AntiForgeryConfig.CookieName;
-        static Regex _rx = new Regex("value=\"(?<token>.+)\"", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
-
         public override void OnActionExecuted(System.Web.Http.Filters.HttpActionExecutedContext actionExecutedContext) {
             if (actionExecutedContext == null) {
                 throw new ArgumentNullException("actionContext");
             }
-            var inputElement = AntiForgery.GetHtml().ToString(); //Will generate the cookie, etc...
-            string newHeaderToken = _rx.Match(inputElement.ToString()).Groups["token"].Value;
-            actionExecutedContext.Response.Headers.Add(_tokenKey, newHeaderToken);
+
+            string cookieToken, newCookieToken, headerToken;
+
+            
+            string tokenKey = AntiForgeryConfig.CookieName;
+
+            CookieHeaderValue cookie = actionExecutedContext.Request.Headers.GetCookies(tokenKey).FirstOrDefault();
+            cookieToken = cookie != null ? cookie[tokenKey].Value : null;
+
+            AntiForgery.GetTokens(cookieToken, out newCookieToken, out headerToken);
+            if (newCookieToken != null) {
+                var newCookie = new CookieHeaderValue(tokenKey, newCookieToken);
+                newCookie.HttpOnly = true;
+                if (AntiForgeryConfig.RequireSsl) {
+                    newCookie.Secure = true;
+                }
+                actionExecutedContext.Response.Headers.AddCookies( new CookieHeaderValue[] { newCookie });
+            }
+
+            actionExecutedContext.Response.Headers.Add(tokenKey, headerToken);
+
             return;
         }
     }
